a2d818f835f7b0935551c34380ea910d9ce212db
pdmackinnon <pdmackinnon@acm.org>
2020-10-20 18:55:10 +0100

    clean up card css more to spec

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 
 let d = document;
-let t = d.getElementById("toggle");
-let t2 = d.getElementById("toggle2");
-let flipButton = d.getElementById("flipButton");
 
 
 d.addEventListener("keydown", e => {
-if (e.key === "f") {
-    t.checked = !t.checked;
-    t2.checked = false;
+
+if (e.key === "ArrowLeft") {
+
+    
 }
 
-if (e.key === "s") {
-    t2.checked = !t2.checked;
-    t.checked = false;
-    if (t2.checked) {
-        //hide flipped button 
-        flipButton.style.display = "none";
-    }
-    else {
-        flipButton.style.display = "initial";
-    }
+if (e.key === "ArrowRight") {
+
+
 }
+
 
 });
